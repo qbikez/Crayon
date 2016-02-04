c02ccbf4e0cdd7eec3f49c14862d3dd089f1d576
Jakub Pawłowski <jakub.pawlowski@legimi.com>
2016-02-04 07:39:36 +0100

    added build scripts

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+        
+  });
+
+  grunt.loadNpmTasks('grunt-shell');
+
+  //grunt.registerTask('default', ['jshint']);
+
+};
---
+++ scripts/functions/global.ps1
@@ -0,0 +1,41 @@
+function covertto-pshashtable($obj) {
+    return $obj.psobject.properties | % { $ht = @{} } { $ht[$_.Name] = $_.Value } { $ht }
+}
+
+function find-globaljson($path = $null) {
+    if ($path -ne $null) {
+        if (Test-Path $path) {
+            $i = gi $path
+            if (!$i.psiscontainer) {
+                return $path
+            }
+        }
+    }
+    else {
+        $path = "."
+    }
+
+    $path = (gi $path).FullName
+    do {
+        $p = join-path $path "global.json"
+        if (test-path $p) { return $p }
+        $path = split-path -Parent $path
+    } 
+    while (![string]::IsNullOrEmpty($path))
+}
+
+function parse-globaljson($path = $null) {
+    $path = find-globaljson $path
+
+    $global = get-content $path | out-string | ConvertFrom-Json
+    
+    if ($global.sdk -ne $null) {
+        $p = covertto-pshashtable $global.sdk
+        & dnvm use @p
+    }
+
+    return $global
+}
+
+
+
